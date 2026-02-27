283b57bcfe98bd7c6ad60b98aa8b85765dd2fa13
jairft <jairpg1993@gmail.com>
2026-02-27 16:10:12 -0300

    config: ajusta environments para deploy na nuvem

--- noemare-web/src/environments/environment.ts
+++ noemare-web/src/environments/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
-  production: true, // 👉 Crucial: indica ao Angular que é modo produção
-  
-  // 👉 Aqui entrará a URL que o Render vai te dar para a API
-  // Por enquanto, vamos deixar o padrão que o Render costuma criar:
-  apiUrl: 'https://noemare-api.onrender.com/api', 
-
-  // 👉 Aqui entra a URL que o Render/Vercel vai dar para o seu Front-end
-  mobileUrl: 'https://noemare-web.onrender.com/login-mobile' 
+  production: true,
+  // 👉 Essa URL o Render vai gerar depois, mas já deixe o padrão:
+  apiUrl: 'https://projeto-noemare.onrender.com/api',
+  mobileUrl: 'https://projeto-noemare.onrender.com/login-mobile'
 };
