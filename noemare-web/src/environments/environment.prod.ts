14f300539d0f31510508d5af0cdae7634b6fbae2
jairft <jairpg1993@gmail.com>
2026-02-27 18:50:45 -0300

    feat: adiciona arquivo de ambiente de produção no caminho correto

---
+++ noemare-web/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // Link da sua API no Render (Back-end)
+  apiUrl: 'https://noemare-api.onrender.com/api',
+  
+  // Link da sua tela de login mobile no Render (Front-end)
+  // 👉 Note que aponta para o "noemare-web"
+  mobileUrl: 'https://noemare-web.onrender.com/login-mobile'
+};
